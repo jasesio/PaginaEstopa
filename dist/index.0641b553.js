af55920379e7631d84de1fcf957b741024ae8087
jose asesio sanchez <jasesio@uoc.edu>
2024-03-28 13:53:29 +0100

    subir ultimos cambios

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -585,6 +585,15 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"bNKaB":[function(require,module,exports) {
 var _scriptTs = require("./script.ts");
+var imagenes = document.querySelectorAll(".img-fluid");
+imagenes.forEach(function(imagen) {
+    imagen.addEventListener("mouseover", function() {
+        imagen.style.border = "4px solid #ff0000";
+    });
+    imagen.addEventListener("mouseout", function() {
+        imagen.style.border = "2px solid transparent";
+    });
+});
 
 },{"./script.ts":"gg0zR"}],"gg0zR":[function(require,module,exports) {
 document.addEventListener("DOMContentLoaded", function() {
