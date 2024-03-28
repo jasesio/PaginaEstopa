74929746ef7a4a6ed4b300db4c7ffa3715c05308
jose asesio sanchez <jasesio@uoc.edu>
2024-03-28 10:29:53 +0100

    funcion javaScript para marcar borde de las fotos

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 import "./script.ts";
+
+var imagenes = document.querySelectorAll('.img-fluid');
+
+imagenes.forEach(function(imagen) {
+  imagen.addEventListener('mouseover', function() {
+    imagen.style.border = '4px solid #ff0000';
+  });
+  imagen.addEventListener('mouseout', function() {
+    imagen.style.border = '2px solid transparent';
+  });
+});
